0f26aa958408c820b26a86f7129cd0968b5cd764
Samara Porto <sp.samaraporto@gmail.com>
2025-02-17 13:01:38 -0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,22 +1,17 @@
 import os
 import requests
-
-# Pega a chave da API do GitHub Secrets
+#pega a chave da api do GitHub Secrets
 API_KEY = os.getenv("API_KEY")
-CITY = os.getenv("CITY", "São Paulo")
+CITY = os.getenv("CITY", "São Paulo") #variavel definida no arquivo de workflow (.yml)
 
 if not API_KEY:
     print("Erro: API_KEY não encontrada! Defina um secret no GitHub.")
     exit(1)
-
-# Chama a API OpenWeather para obter a previsão do tempo
 url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric&lang=pt"
 response = requests.get(url)
-
 if response.status_code != 200:
     print("Erro ao obter dados da API.")
     exit(1)
-
 data = response.json()
 weather = data["weather"][0]["description"]
 temp = data["main"]["temp"]
